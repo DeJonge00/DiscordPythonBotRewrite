c1d42b1e11eee6d44563cdaebd10d30e464d0140
Rick <dejonge00@live.nl>
2019-07-16 16:49:26 +0200

    Added >newpp

--- PythonBot/commands/admin.py
+++ PythonBot/commands/admin.py
@@ -1,8 +1,15 @@
 from core.setup import get_cogs
 from core.bot import PythonBot
+from config import constants
 
+from discord import HTTPException
 from discord.ext import commands
 from discord.ext.commands import Cog, Context
+
+from io import BytesIO
+from os import remove
+from PIL import Image
+import requests
 
 
 # Mod commands
@@ -10,6 +17,39 @@ class AdminCommands(Cog):
     def __init__(self, my_bot: PythonBot):
         self.bot = my_bot
         print('Admin commands cog started')
+
+    @commands.command(name='newpp', hidden=True, help="Give me a new look")
+    async def newpp(self, ctx: Context):
+        if not await self.bot.pre_command(message=ctx.message, channel=ctx.channel, command='newpp', owner_check=True,
+                                          delete_message=False):
+            return
+
+        if len(ctx.message.attachments) > 0:
+            url = ctx.message.attachments[0].url
+        elif len(ctx.message.mentions) > 0:
+            url = '.'.join(ctx.message.mentions[0].avatar_url.split('.')[:-1]) + '.png'
+        else:
+            await self.bot.send_message(ctx.channel, 'Pls give me smth...')
+            return
+        name = 'temp/newpp.png'
+        try:
+            # TODO: Fix saving pic first
+            Image.open(BytesIO(requests.get(url).content)).save(name)
+            await self.bot.delete_message(ctx.message)
+            with open(name, 'rb') as file:
+                await self.bot.user.edit(avatar=file.read())
+            await self.bot.send_message(ctx.channel, 'Make-up is done sweety <3')
+        except requests.exceptions.MissingSchema as e:
+            await self.bot.send_message(ctx.channel, 'Thats not a valid target' + str(e))
+        except OSError as e:
+            await self.bot.send_message(ctx.channel, 'Thats not a valid target' + str(e))
+        except HTTPException:
+            await self.bot.send_message(ctx.channel, 'Discord pls')
+        finally:
+            try:
+                remove(name)
+            except:
+                pass
 
     @commands.command(name='serverlist', hidden=1, help="List the servers the bot can see", aliases=['guildlist'])
     async def serverlist(self, ctx):
