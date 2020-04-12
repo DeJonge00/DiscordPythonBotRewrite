cc5a870a05a8e687d5e02d2553c027ccbd626313
Rick <dejonge00@live.nl>
2020-04-12 16:42:03 +0200

    Delete duplicate requirements.txt

--- PythonBot/api/commands.py
+++ PythonBot/api/commands.py
@@ -9,6 +9,6 @@ from secret.api_secrets import route_start
 def init_commands(api: Flask, auth: HTTPBasicAuth):
     @api.route(route_start + '/commands', methods=['GET'])
     @auth.login_required
-    def get_command_counters(owo):
+    def get_command_counters():
         t = datetime.now().timestamp() - (24 * 60 * 60)  # Limit to 2 weeks of data
         return jsonify(list(get_command_counters(t)))
